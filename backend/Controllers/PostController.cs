fd2e1ceac32f4abe7f3ef030b82197aba4f58ef7
Tias Fenech <tiasfenech@gmail.com>
2021-01-02 12:27:04 +0100

    First backend commit

---
+++ backend/Controllers/PostController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PostController : ControllerBase
+    {
+        private static readonly Post[] _posts = new Post[] {
+            new Post{Body = "This is my first post", Title = "First Post", Category = "Technology", Date = DateTime.Now, Summary = "Summary of my first post"},
+            new Post{Body = "This is my second post", Title = "Second Post", Category = "Books", Date = DateTime.Now, Summary = "Summary of my second post"},
+            new Post{Body = "This is my third post", Title = "Third Post", Category = "Algorithms", Date = DateTime.Now, Summary = "Summary of my third post"},
+            new Post{Body = "This is my forth post", Title = "Forth Post", Category = "Knowledge", Date = DateTime.Now, Summary = "Summary of my forth post"},
+            new Post{Body = "This is my fifth post", Title = "Fifth Post", Category = "Technology", Date = DateTime.Now, Summary = "Summary of my fifth post"},
+            new Post{Body = "This is my sixth post", Title = "Sixth Post", Category = "Books", Date = DateTime.Now, Summary = "Summary of my sixth post"},
+            new Post{Body = "This is my seventh post", Title = "Seventh Post", Category = "Algorithms", Date = DateTime.Now, Summary = "Summary of my seventh post"},
+            new Post{Body = "This is my eight post", Title = "Eight Post", Category = "Knowledge", Date = DateTime.Now, Summary = "Summary of my eight post"},
+            new Post{Body = "This is my ninth post", Title = "Ninth Post", Category = "Technology", Date = DateTime.Now, Summary = "Summary of my ninth post"},
+            new Post{Body = "This is my tenth post", Title = "Tenth Post", Category = "Books", Date = DateTime.Now, Summary = "Summary of my tenth post"},
+            new Post{Body = "This is my eleventh post", Title = "Eleventh Post", Category = "Algorithms", Date = DateTime.Now, Summary = "Summary of my eleventh post"},
+            new Post{Body = "This is my twelfth post", Title = "Twelfth Post", Category = "Knowledge", Date = DateTime.Now, Summary = "Summary of my twelfth post"},
+        };
+        private readonly ILogger<PostController> _logger;
+
+        public PostController(ILogger<PostController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Post> GetAll()
+        {
+            return _posts;
+        }
+    }
+}
---
+++ backend/Posts.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace backend
+{
+    public class Post
+    {
+        public DateTime Date { get; set; }
+
+        public string Summary { get; set; }
+        public string Category {get;set;}
+        public string Body {get;set;}
+        public string Title {get;set;}
+    }
+}
