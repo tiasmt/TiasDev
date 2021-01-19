a930dec55502a19a8afe762a5e50aa178048a353
Tias Fenech <tiasfenech@gmail.com>
2021-01-19 22:27:55 +0100

    Fixed base URL

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ Vue.config.productionTip = false
 
 let baseUrl = ''
 if (process.env.NODE_ENV === 'production') {
-   baseUrl = 'https://tiasdev-backend.herokuapp.com/';
+   baseUrl = 'https://tiasdev-backend.herokuapp.com/rest/api/latest';
 } else {
    baseUrl = 'https://localhost:5001/rest/api/latest';
 }
